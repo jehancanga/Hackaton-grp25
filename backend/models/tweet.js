74d1a55a145187eca12b7da4dd55d33678af40bf
emiliedelrue <emilie_delrue@orange.fr>
2025-03-13 09:33:50 +0100

    Change fichiers

--- backend/models/tweet.js
+++ backend/models/tweet.js
@@ -1,54 +1,17 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const TweetSchema = new mongoose.Schema({
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const tweetSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true, maxlength: 280 },
+    media: { type: String, default: "" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    retweets: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    hashtags: [{ type: String }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   },
-  content: {
-    type: String,
-    required: true,
-    maxlength: 280
-  },
-  media: [{
-    type: String  // URL des médias (images/vidéos)
-  }],
-  hashtags: [{
-    type: String
-  }],
-  mentions: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  retweets: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  replies: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Tweet'
-  }],
-  emotionStats: {
-    joy: { type: Number, default: 0 },
-    sadness: { type: Number, default: 0 },
-    anger: { type: Number, default: 0 },
-    surprise: { type: Number, default: 0 },
-    disgust: { type: Number, default: 0 },
-    fear: { type: Number, default: 0 },
-    neutral: { type: Number, default: 0 }
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
-// Indexer pour les recherches rapides
-TweetSchema.index({ content: 'text', hashtags: 'text' });
-
-module.exports = mongoose.model('Tweet', TweetSchema);
+const Tweet = mongoose.model("Tweet", tweetSchema);
+export default Tweet;
