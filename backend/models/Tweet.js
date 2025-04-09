4d83e4f8b7c56ceed77525caf216eb9f98843598
jehancanga <jehancanga@gmail.com>
2025-04-09 11:04:16 +0200

    Update Tweet.js

--- backend/models/Tweet.js
+++ backend/models/Tweet.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-// Update the tweet schema to include category and detectedEmotion fields
-
 const tweetSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
@@ -11,12 +9,6 @@ const tweetSchema = new mongoose.Schema(
     retweets: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hashtags: [{ type: String }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-    category: { type: String, enum: [
-      "Technology", "Science", "Sports", "Entertainment", "Politics", 
-      "Health", "Education", "Business", "Travel", "Food", 
-      "Fashion", "Music", "Art", "Gaming", "Environment", "Uncategorized"
-    ], default: "Uncategorized" },
-    detectedEmotion: { type: String, enum: ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"], default: "neutral" }
   },
   { timestamps: true }
 );
